bd941bfb555d06b3bb188b7befd04c110e69783d
Сергей Сенин <senin.s.s@yandex.ru>
2020-10-13 12:31:27 +0300

    Update UserService.java

--- src/main/java/com/javamentor/qa/platform/service/impl/model/UserService.java
+++ src/main/java/com/javamentor/qa/platform/service/impl/model/UserService.java
@@ -10,19 +10,5 @@ public class UserService extends ReadWriteServiceImpl<User, Long> {
     public UserService(ReadOnlyDaoImpl readOnlyDao, ReadWriteDaoImpl<User, Long> readWriteDao) {
         super(readOnlyDao, readWriteDao);
     }
-
-    @Override
-    public void persist(User user) {
-        super.persist(user);
-    }
-
-    @Override
-    public void update(User user) {
-        super.update(user);
-    }
-
-    @Override
-    public void delete(User user) {
-        super.delete(user);
-    }
+    
 }
