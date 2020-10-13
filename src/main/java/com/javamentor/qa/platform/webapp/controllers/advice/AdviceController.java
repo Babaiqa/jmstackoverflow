b95f87fac4476de1c8cfcfe531536ba522127301
Vadim Bridnya <v-bridnya@yandex.ru>
2020-10-13 20:49:15 +0300

    Advance Controller second commit refactor after code review

--- src/main/java/com/javamentor/qa/platform/webapp/controllers/advice/AdviceController.java
+++ src/main/java/com/javamentor/qa/platform/webapp/controllers/advice/AdviceController.java
@@ -3,30 +3,27 @@ package com.javamentor.qa.platform.webapp.controllers.advice;
 
 import com.javamentor.qa.platform.exception.ConstrainException;
 import org.hibernate.exception.ConstraintViolationException;
-import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
+
 import javax.persistence.EntityNotFoundException;
 
 @ControllerAdvice
 public class AdviceController {
 
     @ExceptionHandler(ConstraintViolationException.class)
-    public ResponseEntity<String> handleConstraintViolationException() {
-        String message = "Invalid database query";
-        return new ResponseEntity<>(message, HttpStatus.BAD_REQUEST);
+    public static ResponseEntity<String> handleConstraintViolationException(ConstraintViolationException e) {
+         return  ResponseEntity.badRequest().header(e.getMessage()).build();
     }
 
     @ExceptionHandler(ConstrainException.class)
-    public ResponseEntity<String> handleConstrainException() {
-        String message = "Problem";
-        return new ResponseEntity<>(message, HttpStatus.BAD_REQUEST);
+    public ResponseEntity<String> handleConstrainException(ConstrainException e) {
+
+        return ResponseEntity.badRequest().header(e.getMessage()).build();
     }
     @ExceptionHandler(EntityNotFoundException.class)
-    public ResponseEntity<String> handleEntityNotFoundException() {
-        String message = "Entity not found";
-        return new ResponseEntity<>(message, HttpStatus.BAD_REQUEST);
+    public ResponseEntity<String> handleEntityNotFoundException(EntityNotFoundException e) {
+        return ResponseEntity.badRequest().header(e.getMessage()).build();
     }
-
 }
