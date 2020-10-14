86aab72c5b1d08a24de0886834450dc2f03d6c45
monster <nexx01@mail.ru>
2020-10-14 14:44:46 +0300

    реализовал методы updateAll deleteAll persistAll

--- src/main/java/com/javamentor/qa/platform/dao/impl/model/ReadWriteDaoImpl.java
+++ src/main/java/com/javamentor/qa/platform/dao/impl/model/ReadWriteDaoImpl.java
@@ -1,5 +1,7 @@
 package com.javamentor.qa.platform.dao.impl.model;
 
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.transaction.annotation.Transactional;
 
 import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
@@ -8,8 +10,12 @@ import java.util.Collection;
 
 public abstract class ReadWriteDaoImpl<E, K> extends ReadOnlyDaoImpl<E, K> {
 
+    @Value("${spring.jpa.properties.hibernate.jdbc.batch_size}")
+    private int batchSize;
+
     @PersistenceContext
     private EntityManager entityManager;
+
 
     public void persist(E e) {
         entityManager.persist(e);
@@ -24,18 +30,79 @@ public abstract class ReadWriteDaoImpl<E, K> extends ReadOnlyDaoImpl<E, K> {
     }
 
     public void persistAll(E... entities) {
+        int i = 0;
+
+        for (E entity : entities) {
+            entityManager.persist(entity);
+
+            i++;
+
+            // Flush a batch of inserts and release memory
+            if (i % batchSize == 0 && i > 0) {
+
+                entityManager.flush();
+                entityManager.clear();
+                i = 0;
+            }
+        }
+        if (i > 0) {
+            entityManager.flush();
+            entityManager.clear();
+        }
 
     }
 
     public void persistAll(Collection<E> entities) {
+        int i = 0;
 
+        for (E entity : entities) {
+            entityManager.persist(entity);
+
+            i++;
+
+            // Flush a batch of inserts and release memory
+            if (i % batchSize == 0 && i > 0) {
+
+                entityManager.flush();
+                entityManager.clear();
+                i = 0;
+            }
+        }
+        if (i > 0) {
+            entityManager.flush();
+            entityManager.clear();
+        }
     }
 
+
+    @Transactional
     public void deleteAll(Collection<E> entities) {
-
+        for (E entity : entities) {
+            entityManager.remove(entityManager.contains(entity) ? entity : entityManager.merge(entity));
+        }
     }
+
 
     public void updateAll(Iterable<? extends E> entities) {
+        int i = 0;
 
+        for (E entity : entities) {
+            entityManager.merge(entity);
+
+            i++;
+
+            // Flush a batch of inserts and release memory
+            if (i % batchSize == 0 && i > 0) {
+
+                entityManager.flush();
+                entityManager.clear();
+                i = 0;
+            }
+        }
+        if (i > 0) {
+            entityManager.flush();
+            entityManager.clear();
+        }
     }
+
 }
