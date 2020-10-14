425b1b9da93ef760fe8b971b3721a7c15a60c54c
arigelis <granistad@gmail.com>
2020-10-14 19:32:27 +0500

    ReadOnlyDaoImpl_getAllByIds_v1

--- src/main/java/com/javamentor/qa/platform/dao/impl/model/ReadOnlyDaoImpl.java
+++ src/main/java/com/javamentor/qa/platform/dao/impl/model/ReadOnlyDaoImpl.java
@@ -1,10 +1,16 @@
 package com.javamentor.qa.platform.dao.impl.model;
 
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+import java.lang.reflect.ParameterizedType;
 import java.util.Collection;
 import java.util.List;
 import java.util.Optional;
 
 public abstract class ReadOnlyDaoImpl<E, K> {
+
+    @PersistenceContext
+    private EntityManager entityManager;
 
     public List<E> getAll() {
         return null;
@@ -19,10 +25,16 @@ public abstract class ReadOnlyDaoImpl<E, K> {
     }
 
     public List<E> getAllByIds(Iterable<K> ids) {
-        return null;
+        Class genericClass = getCurrentGenericClass();
+        return entityManager.createQuery("SELECT e FROM genericClass e WHERE e.id IN :ids", genericClass).setParameter("ids", ids).getResultList();
     }
 
     public boolean existsByAllIds(Collection<K> ids) {
         return false;
+    }
+
+    private Class getCurrentGenericClass() {
+        return (Class<E>) ((ParameterizedType) getClass().getGenericSuperclass())
+                .getActualTypeArguments()[0];
     }
 }
