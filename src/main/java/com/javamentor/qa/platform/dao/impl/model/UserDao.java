bddd18499dea502abd9a8132e068e7b31549cdb5
Vadim Bridnya <v-bridnya@yandex.ru>
2020-10-15 23:44:28 +0300

    UserController commit add package mappers, refactor UserDao: add method getUserByEmail

--- src/main/java/com/javamentor/qa/platform/dao/impl/model/UserDao.java
+++ src/main/java/com/javamentor/qa/platform/dao/impl/model/UserDao.java
@@ -3,7 +3,18 @@ package com.javamentor.qa.platform.dao.impl.model;
 import com.javamentor.qa.platform.models.entity.user.User;
 import org.springframework.stereotype.Repository;
 
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+
 @Repository
 public class UserDao extends ReadWriteDaoImpl<User, Long> {
+    @PersistenceContext
+    private EntityManager entityManager;
 
+    public User getUserByEmail(String email) {
+        String query = "SELECT  from User  where email = " + email;
+        User user;
+        user = (User) entityManager.createQuery(query).getSingleResult();
+        return user;
+    }
 }
--- src/main/java/com/javamentor/qa/platform/webapp/controllers/UserController.java
+++ src/main/java/com/javamentor/qa/platform/webapp/controllers/UserController.java
@@ -1,18 +1,34 @@
 package com.javamentor.qa.platform.webapp.controllers;
 
+import com.javamentor.qa.platform.dao.impl.model.UserDao;
+import com.javamentor.qa.platform.mappers.UserMapper;
+import com.javamentor.qa.platform.models.dto.UserDto;
+import com.javamentor.qa.platform.models.dto.UserRegistrationDto;
+import com.javamentor.qa.platform.models.entity.user.Role;
+import com.javamentor.qa.platform.models.entity.user.User;
+import com.javamentor.qa.platform.service.impl.model.UserService;
 import io.swagger.annotations.*;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.annotation.Validated;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @Validated
 @RequestMapping("/api/user/")
 @Api(value = "UserApi")
 public class UserController {
+
+    private final UserService userService;
+
+    private final UserDao userDao;
+
+    @Autowired
+    public UserController(UserService userService, UserDao userDao) {
+        this.userService = userService;
+        this.userDao = userDao;
+    }
 
 
     // Examples for Swagger
@@ -24,10 +40,30 @@ public class UserController {
     })
     public  ResponseEntity<String> getUserById(
           @ApiParam(name="id",value="type Long(or other descriped)", required = true, example="0")
-            @PathVariable Long id){
+          @PathVariable Long id){
 
-        return id!=null ? ResponseEntity.ok("Swager work"):
+        return id!=null ? ResponseEntity.ok("Swagger work"):
                 ResponseEntity.badRequest()
                         .body("Wrong ID");
+   }
+
+    @PostMapping(value = "registration", consumes = MediaType.APPLICATION_JSON_VALUE, produces =
+           MediaType.APPLICATION_JSON_VALUE)
+    public ResponseEntity<String> createUser(@RequestBody UserRegistrationDto userRegistrationDto) {
+        String email = userRegistrationDto.getEmail();
+        if (userDao.getUserByEmail(email) != null) {
+            String message = "Пользователь с данным email уже существует";
+            return ResponseEntity.badRequest().header(message).build();
+        } else {
+            User user = UserMapper.INSTANCE.userRegistrationDtoToUser(userRegistrationDto);
+            Role role = null;
+            assert false;
+            role.setName("USER");
+            user.setRole(role);
+            userService.persist(user);
+            User us = userDao.getUserByEmail(email);
+            UserDto userDto = UserMapper.INSTANCE.userToUserDto(us);
+            return ResponseEntity.ok().body(userDto.toString());
+        }
    }
 }
