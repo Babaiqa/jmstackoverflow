8dc71c26ffb8708bd8e1fe75902bea10418e54a0
kurortnik00 <kurortnik00@gmail.com>
2020-10-15 16:52:35 +0300

    existsById implementation

--- src/main/java/com/javamentor/qa/platform/dao/impl/model/ReadOnlyDaoImpl.java
+++ src/main/java/com/javamentor/qa/platform/dao/impl/model/ReadOnlyDaoImpl.java
@@ -1,17 +1,33 @@
 package com.javamentor.qa.platform.dao.impl.model;
 
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+import java.lang.reflect.ParameterizedType;
 import java.util.Collection;
 import java.util.List;
 import java.util.Optional;
 
 public abstract class ReadOnlyDaoImpl<E, K> {
 
+    @PersistenceContext
+    private EntityManager entityManager;
+
+    protected Class<E> genericClass;
+
+    public ReadOnlyDaoImpl() {
+        this.genericClass = (Class<E>) ((ParameterizedType) getClass()
+                .getGenericSuperclass())
+                .getActualTypeArguments()[0];
+    }
+
+
     public List<E> getAll() {
         return null;
     }
 
     public boolean existsById(K id) {
-        return false;
+        int count = (int) entityManager.createQuery("SELECT COUNT(e) FROM " + genericClass.getName() + " e WHERE e.id =: id").getSingleResult();
+        return count == 1;
     }
 
     public Optional<E> getById(K id) {
