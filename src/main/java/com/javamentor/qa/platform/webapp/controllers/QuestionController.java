6cf5468072d156d9a7f76775b139deb5d8fda9fc
NadejdaDoborina <profiwoodru@gmail.com>
2020-10-15 18:09:53 +0300

    Build fixed

--- src/main/java/com/javamentor/qa/platform/webapp/controllers/QuestionController.java
+++ src/main/java/com/javamentor/qa/platform/webapp/controllers/QuestionController.java
@@ -1,33 +1,23 @@
 package com.javamentor.qa.platform.webapp.controllers;
 
-import com.javamentor.qa.platform.models.entity.question.Question;
-import com.javamentor.qa.platform.service.abstracts.dto.QuestionDtoService;
 import com.javamentor.qa.platform.service.impl.model.QuestionService;
-import com.javamentor.qa.platform.webapp.converters.QuestionConverter;
 import io.swagger.annotations.*;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
 
-import java.util.Optional;
-
 @RestController
 @Validated
 @RequestMapping("/api/question/")
 @Api(value = "QuestionApi")
 public class QuestionController {
-    private QuestionConverter questionConverter;
     private QuestionService questionService;
-    private QuestionDtoService questionDtoService;
 
-    @Autowired
-    public QuestionController(QuestionConverter questionConverter,
-                              QuestionService questionService, QuestionDtoService questionDtoService){
-        this.questionConverter = questionConverter;
-        this.questionService = questionService;
-        this.questionDtoService = questionDtoService;
-    }
+@Autowired
+public QuestionController(QuestionService questionService) {
+    this.questionService = questionService;
+}
 
     @DeleteMapping("/{id}/delete")
     @ApiOperation(value = "Delete question", response = String.class)
