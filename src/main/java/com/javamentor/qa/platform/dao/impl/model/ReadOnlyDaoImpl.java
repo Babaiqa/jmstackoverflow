953d31a38f03fc582e787cd4142264d72fdc0b0d
arigelis <granistad@gmail.com>
2020-10-16 14:09:33 +0500

    without conflicts

--- src/main/java/com/javamentor/qa/platform/dao/impl/model/ReadOnlyDaoImpl.java
+++ src/main/java/com/javamentor/qa/platform/dao/impl/model/ReadOnlyDaoImpl.java
@@ -7,6 +7,7 @@ import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
 import javax.persistence.TypedQuery;
 import java.lang.reflect.ParameterizedType;
+import java.util.ArrayList;
 import java.util.Collection;
 import java.util.List;
 import java.util.Optional;
@@ -32,7 +33,14 @@ public abstract class ReadOnlyDaoImpl<E, K> {
     }
 
     public List<E> getAllByIds(Iterable<K> ids) {
-        return null;
+        Class<E> clazz = (Class<E>) ((ParameterizedType) getClass().getGenericSuperclass())
+                .getActualTypeArguments()[0];
+        if (ids != null && ids.iterator().hasNext()) {
+            return entityManager.createQuery("from " + clazz.getName() + " e WHERE e.id IN :ids")
+                    .setParameter("ids", ids).getResultList();
+        } else {
+            return new ArrayList<>();
+        }
     }
 
     public boolean existsByAllIds(Collection<K> ids) {
