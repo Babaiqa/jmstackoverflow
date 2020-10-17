21632294f5a755f7bcd7df2871ec1a451527bff3
i210350 <i210350@yandex.ru>
2020-10-17 09:57:25 +0300

    api_add_question

--- src/main/java/com/javamentor/qa/platform/webapp/converters/QuestionConverter.java
+++ src/main/java/com/javamentor/qa/platform/webapp/converters/QuestionConverter.java
@@ -2,15 +2,8 @@ package com.javamentor.qa.platform.webapp.converters;
 
 import com.javamentor.qa.platform.models.dto.QuestionDto;
 import com.javamentor.qa.platform.models.entity.question.Question;
-import org.springframework.beans.BeanUtils;
 
-public class QuestionConverter implements QuestionDtoMapper{
-
-    public Question questionToDto(QuestionDto questionDto) {
-        return null;
-    }
-
-    public QuestionDto dtoToQuestion(Question question) {
-        return null;
-    }
+public interface QuestionConverter {
+    QuestionDto questionToDto(Question question);
+    Question dtoToQuestion(QuestionDto questionDto);
 }
