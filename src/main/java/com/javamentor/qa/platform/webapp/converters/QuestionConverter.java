e1b20fee33b92bf4ab76dcb3988471ec445a4006
i210350 <i210350@yandex.ru>
2020-10-17 19:57:18 +0300

    api_add_question

--- src/main/java/com/javamentor/qa/platform/webapp/converters/QuestionConverter.java
+++ src/main/java/com/javamentor/qa/platform/webapp/converters/QuestionConverter.java
@@ -2,10 +2,22 @@ package com.javamentor.qa.platform.webapp.converters;
 
 import com.javamentor.qa.platform.models.dto.QuestionDto;
 import com.javamentor.qa.platform.models.entity.question.Question;
-import org.mapstruct.Mapper;
+import org.mapstruct.*;
 
 @Mapper
 public interface QuestionConverter {
-    QuestionDto questionToDto(Question question);
-    Question dtoToQuestion(QuestionDto questionDto);
+
+        @Mapping(source="question.id",target="id")
+        @Mapping(source="question.title",target="title")
+
+
+QuestionDto questionToQuestionDto(Question question);
+
+
+            @Mapping(source="questionDto.id",target="id")
+            @Mapping(source="questionDto.title",target="title")
+
+
+    Question questionDtoToQuestion(QuestionDto questionDto);
+
 }
