73b1a9d740918bd4c6c9539f913ada821efb68d0
monster <nexx01@mail.ru>
2020-10-19 11:12:10 +0300

    add alias

--- src/main/java/com/javamentor/qa/platform/models/dto/TagDto.java
+++ src/main/java/com/javamentor/qa/platform/models/dto/TagDto.java
@@ -6,6 +6,9 @@ import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 import java.io.Serializable;
+import java.util.Map;
+
+import static ognl.OgnlOps.stringValue;
 
 @Getter
 @Setter
@@ -13,8 +16,19 @@ import java.io.Serializable;
 @NoArgsConstructor
 public class TagDto implements Serializable {
 
+    public static final String ID_ALIAS = "t_id";
+    public static final String NAME_ALIAS = "t_name";
+
     private static final long serialVersionUID = -8087563918115872879L;
     private Long id;
 
     private String name;
+
+    public TagDto(
+            Object[] tuples,
+            Map<String, Integer> aliasToIndexMap) {
+        this.id = ((Number) tuples[aliasToIndexMap.get(ID_ALIAS)]).longValue();
+        this.name = tuples[aliasToIndexMap.get(NAME_ALIAS)].toString();
+    }
+
 }
