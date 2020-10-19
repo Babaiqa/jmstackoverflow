dd817de2aca15c9b17a181630caa6bbff5a81cc4
monster <nexx01@mail.ru>
2020-10-19 11:13:17 +0300

    add getQuestionById

--- src/main/java/com/javamentor/qa/platform/webapp/controllers/QuestionController.java
+++ src/main/java/com/javamentor/qa/platform/webapp/controllers/QuestionController.java
@@ -1,9 +1,7 @@
 package com.javamentor.qa.platform.webapp.controllers;
 
 import com.javamentor.qa.platform.models.dto.QuestionDto;
-import com.javamentor.qa.platform.models.entity.question.Question;
-import com.javamentor.qa.platform.service.impl.model.QuestionServise;
-import com.javamentor.qa.platform.webapp.converters.QuestionConverter;
+import com.javamentor.qa.platform.service.impl.dto.QuestionDtoService;
 import io.swagger.annotations.*;
 import lombok.RequiredArgsConstructor;
 
@@ -13,8 +11,6 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-
-import javax.persistence.EntityNotFoundException;
 import java.util.Optional;
 
 @RestController
@@ -24,28 +20,23 @@ import java.util.Optional;
 @RequiredArgsConstructor
 public class QuestionController {
 
-    private final QuestionServise questionServise;
-    private final QuestionConverter questionConverter;
+    private final QuestionDtoService questionDtoService;
 
 
-    // Examples for Swagger
     @GetMapping("{id}")
     @ApiOperation(value = "get QuestionDto", response = String.class)
     @ApiResponses({
-            @ApiResponse(code = 200, message = "Returns the QuestionDto.", response = QuestionDto.class),
+            @ApiResponse(code = 200, message = "Returns the QuestionDto", response = QuestionDto.class),
             @ApiResponse(code = 400, message = "Question not found", response = String.class)
     })
 
-    public ResponseEntity<QuestionDto> getQuestionById(
+    public ResponseEntity<?> getQuestionById(
             @ApiParam(name = "id", value = "type Long", required = true, example = "0")
             @PathVariable Long id) {
-        Optional<Question> question = questionServise.getById(id);
 
-        if (question.isPresent()) {
-            return ResponseEntity.ok(questionConverter.questionToQuestionDTO(question.get()));
+        Optional<QuestionDto> questionDto = questionDtoService.getQuestionDtoById(id);
 
-        } else {
-            throw new EntityNotFoundException("Question not found");
-        }
+        return questionDto.isPresent()?ResponseEntity.ok(questionDto.get()):
+                ResponseEntity.badRequest().body("Question not found");
     }
 }
