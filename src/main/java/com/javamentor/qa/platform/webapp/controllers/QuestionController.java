6c01c4c1b544137620009b86c54d4a82926b0a58
NadejdaDoborina <profiwoodru@gmail.com>
2020-10-19 19:58:10 +0300

    Small fixes based on testing result

--- src/main/java/com/javamentor/qa/platform/webapp/controllers/QuestionController.java
+++ src/main/java/com/javamentor/qa/platform/webapp/controllers/QuestionController.java
@@ -1,6 +1,8 @@
 package com.javamentor.qa.platform.webapp.controllers;
 
-import com.javamentor.qa.platform.service.impl.model.QuestionService;
+import com.javamentor.qa.platform.models.dto.QuestionDto;
+import com.javamentor.qa.platform.service.impl.model.QuestionServiceImpl;
+import com.javamentor.qa.platform.webapp.converters.QuestionConverter;
 import io.swagger.annotations.*;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
@@ -12,11 +14,13 @@ import org.springframework.web.bind.annotation.*;
 @RequestMapping("/api/question/")
 @Api(value = "QuestionApi")
 public class QuestionController {
-    private QuestionService questionService;
+    private QuestionServiceImpl questionServiceImpl;
+    private QuestionDto questionDto;
+    private QuestionConverter questionConverter;
 
 @Autowired
-public QuestionController(QuestionService questionService) {
-    this.questionService = questionService;
+public QuestionController(QuestionServiceImpl questionServiceImpl) {
+    this.questionServiceImpl = questionServiceImpl;
 }
 
     @DeleteMapping("/{id}/delete")
@@ -27,11 +31,27 @@ public QuestionController(QuestionService questionService) {
     })
     public ResponseEntity<String> deleteQuestionById(@ApiParam(name = "id") @PathVariable Long id) {
 
-        if(questionService.existsById(id)) {
-            questionService.delete(questionService.getById(id).get());
+        if(Boolean.TRUE.equals(questionServiceImpl.existsById(id)))
+        {
+            questionServiceImpl.deleteById(id);
             return ResponseEntity.ok("Question was deleted");
         } else {
             return ResponseEntity.badRequest().body("Wrong ID");
         }
+    }
+
+    @GetMapping("{id}")
+    @ApiOperation(value = "Return message(Object)", response = String.class)
+    @ApiResponses({
+            @ApiResponse(code = 200, message = "Returns the object.", response = String.class),
+            @ApiResponse(code = 400, message = "Wrong ID",response = String.class)
+    })
+    public  ResponseEntity<String> getQuestionById(
+            @ApiParam(name="id",value="type Long(or other described)", required = true, example="0")
+            @PathVariable Long id){
+
+        return id!=null ? ResponseEntity.ok("Swagger work"):
+                ResponseEntity.badRequest()
+                        .body("Wrong ID");
     }
 }
