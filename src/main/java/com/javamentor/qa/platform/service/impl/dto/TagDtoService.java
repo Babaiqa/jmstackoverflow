fd6ceb24934573a710e2117fc1be87d2fc65927c
monster <nexx01@mail.ru>
2020-10-19 15:35:50 +0300

    убрал ломбок конструктор

--- src/main/java/com/javamentor/qa/platform/service/impl/dto/TagDtoService.java
+++ src/main/java/com/javamentor/qa/platform/service/impl/dto/TagDtoService.java
@@ -3,17 +3,21 @@ package com.javamentor.qa.platform.service.impl.dto;
 
 import com.javamentor.qa.platform.dao.impl.dto.TagDtoDAO;
 import com.javamentor.qa.platform.models.dto.TagDto;
-import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
 
 @Service
-@RequiredArgsConstructor
+
 public class TagDtoService {
 
     private final TagDtoDAO tagDtoDAO;
 
+    @Autowired
+    public TagDtoService(TagDtoDAO tagDtoDAO) {
+        this.tagDtoDAO = tagDtoDAO;
+    }
 
     public List<TagDto> getTagDtoPagination(int page, int size) {
         return tagDtoDAO.getTagDtoPagination(page,size);
