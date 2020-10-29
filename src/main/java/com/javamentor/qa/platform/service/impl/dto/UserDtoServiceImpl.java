04dd14637e0d17228a3a8515a10958a4c4ac4d40
monster <nexx01@mail.ru>
2020-10-29 18:39:36 +0300

    add getPage<UserDtoList,Object> getPageUserDtoListByReputationOverPeriod

--- src/main/java/com/javamentor/qa/platform/service/impl/dto/UserDtoServiceImpl.java
+++ src/main/java/com/javamentor/qa/platform/service/impl/dto/UserDtoServiceImpl.java
@@ -1,7 +1,9 @@
 package com.javamentor.qa.platform.service.impl.dto;
 
 import com.javamentor.qa.platform.dao.abstracts.dto.UserDtoDao;
+import com.javamentor.qa.platform.models.dto.PageDto;
 import com.javamentor.qa.platform.models.dto.UserDto;
+import com.javamentor.qa.platform.models.dto.UserDtoList;
 import com.javamentor.qa.platform.service.abstracts.dto.UserDtoService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -21,5 +23,21 @@ public class UserDtoServiceImpl implements UserDtoService {
     @Override
     public Optional<UserDto> getUserDtoById(long id) {
         return userDtoDao.getUserById(id);
+    }
+
+    @Override
+    public PageDto<UserDtoList,Object> getPageUserDtoListByReputationOverPeriod(int page, int size){
+
+        PageDto<UserDtoList,Object> pageDto = new PageDto<>();
+
+        int totalResultCount=userDtoDao.getTotalResultCountUsers();
+
+        pageDto.setItems(userDtoDao.getPageUserDtoListByReputationOverPeriod(page, size));
+        pageDto.setTotalResultCount(totalResultCount);
+        pageDto.setCurrentPageNumber(page);
+        pageDto.setItemsOnPage(size);
+        pageDto.setTotalPageCount((int) Math.ceil(totalResultCount/(double)size));
+
+        return pageDto;
     }
 }
