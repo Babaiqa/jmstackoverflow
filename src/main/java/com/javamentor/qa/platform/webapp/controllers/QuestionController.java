f49745d0aeb1c98a0dcdeca27c5d4f9b63ebe3a2
i210350 <i210350@yandex.ru>
2020-11-02 10:45:59 +0300

    1

--- src/main/java/com/javamentor/qa/platform/webapp/controllers/QuestionController.java
+++ src/main/java/com/javamentor/qa/platform/webapp/controllers/QuestionController.java
@@ -22,6 +22,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.Collections;
 import java.util.List;
 import java.util.Optional;
 
