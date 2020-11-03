9fc08faf6cc099b5fa312b2c53c513884013f3dd
monster <nexx01@mail.ru>
2020-11-03 09:01:12 +0300

    init

--- src/main/java/com/javamentor/qa/platform/service/impl/model/CommentQuestionServiceImpl.java
+++ src/main/java/com/javamentor/qa/platform/service/impl/model/CommentQuestionServiceImpl.java
@@ -1,7 +1,10 @@
 package com.javamentor.qa.platform.service.impl.model;
 
 import com.javamentor.qa.platform.dao.abstracts.model.CommentQuestionDao;
+import com.javamentor.qa.platform.models.dto.CommentDto;
 import com.javamentor.qa.platform.models.entity.question.CommentQuestion;
+import com.javamentor.qa.platform.models.entity.question.Question;
+import com.javamentor.qa.platform.models.entity.user.User;
 import com.javamentor.qa.platform.service.abstracts.model.CommentQuestionService;
 import org.springframework.stereotype.Service;
 
@@ -9,5 +12,14 @@ import org.springframework.stereotype.Service;
 public class CommentQuestionServiceImpl extends ReadWriteServiceImpl<CommentQuestion, Long> implements CommentQuestionService {
     public CommentQuestionServiceImpl(CommentQuestionDao commentQuestionDao) {
         super(commentQuestionDao);
+    }
+
+
+    @Override
+    public CommentDto addCommentToQuestion(String commentText, Question question, User user) {
+
+
+
+        return null;
     }
 }
--- src/main/java/com/javamentor/qa/platform/webapp/controllers/CommentController.java
+++ src/main/java/com/javamentor/qa/platform/webapp/controllers/CommentController.java
@@ -1,18 +1,50 @@
 package com.javamentor.qa.platform.webapp.controllers;
 
 
+import com.javamentor.qa.platform.dao.abstracts.dto.UserDtoDao;
+import com.javamentor.qa.platform.models.dto.UserDto;
+import com.javamentor.qa.platform.models.entity.question.Question;
+import com.javamentor.qa.platform.models.entity.user.User;
+import com.javamentor.qa.platform.service.abstracts.dto.UserDtoService;
+import com.javamentor.qa.platform.service.abstracts.model.CommentQuestionService;
+import com.javamentor.qa.platform.service.abstracts.model.QuestionService;
+import com.javamentor.qa.platform.service.abstracts.model.UserService;
+import com.javamentor.qa.platform.service.impl.model.CommentQuestionServiceImpl;
 import io.swagger.annotations.ApiOperation;
 import io.swagger.annotations.ApiResponse;
 import io.swagger.annotations.ApiResponses;
+import org.aspectj.weaver.patterns.TypePatternQuestions;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.Optional;
 
 @RestController
 @Validated
 @RequestMapping("/api/comment/")
 ///api/comment/question/{questionId}
 public class CommentController {
+
+    private final UserDtoService userDtoService;
+    private final UserService userService;
+    private final QuestionService questionService;
+    private final CommentQuestionService commentQuestionService;
+
+
+    @Autowired
+
+    public CommentController(UserDtoService userDtoService, UserService userService, QuestionService questionService, CommentQuestionService commentQuestionService) {
+        this.userDtoService = userDtoService;
+        this.userService = userService;
+        this.questionService = questionService;
+        this.commentQuestionService = commentQuestionService;
+    }
+
+
+
+
 
     @PostMapping(value = "question/{questionId}", params = {"page", "size"})
     @ApiOperation(value = "Add comment to question and return CommentDto")
@@ -21,8 +53,19 @@ public class CommentController {
             @ApiResponse(code = 400, message = "Question not found", response = String.class)
     })
     public ResponseEntity<?> addCommentToQuestion(@PathVariable Long questionId, @RequestParam Long userId,
-                                                  @RequestBody String commentText){
+                                                  @RequestBody String commentText) {
 
+        Optional<User> user = userService.getById(userId);
+        if(!user.isPresent()){
+            return ResponseEntity.badRequest().body("User not found");
+        }
+
+        Optional<Question> question=questionService.getById(questionId);
+        if(!question.isPresent()){
+            return ResponseEntity.badRequest().body("Question not found");
+        }
+
+        commentQuestionService(commentText,question,user);
 
 
 
