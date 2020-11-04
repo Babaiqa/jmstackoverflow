4f600c7419297dec7629df80c06bdea7fa753f9b
RuthenRa <Ruthenra@icloud.com>
2020-11-04 18:27:02 +0300

    "грязные" тесты tagDtoPaginationByPopular

--- src/test/java/com/javamentor/qa/platform/controllers/tag/TagControllerTest.java
+++ src/test/java/com/javamentor/qa/platform/controllers/tag/TagControllerTest.java
@@ -5,8 +5,11 @@ import com.javamentor.qa.platform.AbstractIntegrationTest;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
+import org.springframework.security.crypto.codec.Utf8;
 import org.springframework.test.web.servlet.MockMvc;
 
+
+import static org.hamcrest.Matchers.containsString;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
@@ -20,6 +23,7 @@ public class TagControllerTest extends AbstractIntegrationTest {
     @Autowired
     private MockMvc mockMvc;
 
+    private static final String BAD_REQUEST_MESSAGE = "Номер страницы и размер должны быть положительными. Максимальное количество записей на странице 100";
 
     @Test
     public void correctRequestGetTagDtoPaginationByPopular() throws Exception {
@@ -28,18 +32,53 @@ public class TagControllerTest extends AbstractIntegrationTest {
                 .param("size", "10"))
                 .andDo(print())
                 .andExpect(status().isOk())
-                .andExpect(content().contentTypeCompatibleWith(MediaType.APPLICATION_JSON))
+                .andExpect(content().contentType(MediaType.APPLICATION_JSON))
                 .andExpect(jsonPath("$.items[0].id").value(1))
                 .andExpect(jsonPath("$.items[0].name").value("java"));
     }
 
     @Test
-    public void uncorrectRequestGetTagDtoPaginationByPopular() throws Exception {
+    public void uncorrectRequestGetTagDtoPaginationByPopular1() throws Exception {
         mockMvc.perform(get("/api/tag/popular")
                 .param("page", "0")
                 .param("size", "10"))
                 .andDo(print())
-                .andExpect(status().isBadRequest());
+                .andExpect(status().isBadRequest())
+                .andExpect(content().contentTypeCompatibleWith("text/plain;charset=UTF-8"))
+                .andExpect(content().string(containsString(BAD_REQUEST_MESSAGE)));
+    }
+
+    @Test
+    public void uncorrectRequestGetTagDtoPaginationByPopular2() throws Exception {
+        mockMvc.perform(get("/api/tag/popular")
+                .param("page", "1")
+                .param("size", "0"))
+                .andDo(print())
+                .andExpect(status().isBadRequest())
+                .andExpect(content().contentTypeCompatibleWith("text/plain;charset=UTF-8"))
+                .andExpect(content().string(containsString(BAD_REQUEST_MESSAGE)));
+    }
+
+    @Test
+    public void uncorrectRequestGetTagDtoPaginationByPopular3() throws Exception {
+        mockMvc.perform(get("/api/tag/popular")
+                .param("page", "1")
+                .param("size", "101"))
+                .andDo(print())
+                .andExpect(status().isBadRequest())
+                .andExpect(content().contentTypeCompatibleWith("text/plain;charset=UTF-8"))
+                .andExpect(content().string(containsString(BAD_REQUEST_MESSAGE)));
+    }
+
+    @Test
+    public void uncorrectRequestGetTagDtoPaginationByPopular4() throws Exception {
+        mockMvc.perform(get("/api/tag/popular")
+                .param("page", "13")
+                .param("size", "10"))
+                .andDo(print())
+                .andExpect(status().isOk())
+                .andExpect(content().contentType(MediaType.APPLICATION_JSON))
+                .andExpect(jsonPath("$.items").isEmpty());
     }
 
 }
