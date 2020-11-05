a0a0e4245c4e5c3b294babe4da48cada7ee19e17
monster <nexx01@mail.ru>
2020-11-05 15:28:11 +0300

    remove import

--- src/main/java/com/javamentor/qa/platform/service/impl/model/CommentQuestionServiceImpl.java
+++ src/main/java/com/javamentor/qa/platform/service/impl/model/CommentQuestionServiceImpl.java
@@ -1,6 +1,5 @@
 package com.javamentor.qa.platform.service.impl.model;
 
-import com.javamentor.qa.platform.dao.abstracts.dto.CommentDtoDao;
 import com.javamentor.qa.platform.dao.abstracts.model.CommentQuestionDao;
 import com.javamentor.qa.platform.dao.abstracts.model.ReadWriteDao;
 import com.javamentor.qa.platform.models.entity.Comment;
