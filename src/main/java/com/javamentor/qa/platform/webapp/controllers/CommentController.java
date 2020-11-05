8e700f4faea40ff689706db3d1b240860d4949d8
monster <nexx01@mail.ru>
2020-11-05 12:25:52 +0300

    /

--- src/main/java/com/javamentor/qa/platform/webapp/controllers/CommentController.java
+++ src/main/java/com/javamentor/qa/platform/webapp/controllers/CommentController.java
@@ -52,7 +52,7 @@ public class CommentController {
             @PathVariable Long questionId,
             @ApiParam(name = "userId", value = "userId. Type long", required = true, example = "1")
             @RequestParam Long userId,
-            @ApiParam(name = "text", value = "userId. Type long", required = true, example = "Some comment")
+            @ApiParam(name = "text", value = "userId. Type string", required = true, example = "Some comment")
             @RequestBody String commentText) {
 
         Optional<User> user = userService.getById(userId);
