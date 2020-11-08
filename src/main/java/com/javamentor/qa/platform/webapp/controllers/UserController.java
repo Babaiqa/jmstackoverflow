f3df69859f9efc33424dbcd70e56a6ea89eb9e09
monster <nexx01@mail.ru>
2020-11-08 17:54:22 +0300

    .

--- src/main/java/com/javamentor/qa/platform/webapp/controllers/UserController.java
+++ src/main/java/com/javamentor/qa/platform/webapp/controllers/UserController.java
@@ -74,7 +74,7 @@ public class UserController {
                     response = List.class),
             @ApiResponse(code = 400, message = "Wrong value of size or page", response = String.class)
     })
-    public ResponseEntity<?> getUserDtoPaginationByReputationOverWeek(
+    public ResponseEntity<?> getUserDtoListPaginationByReputationOverWeek(
             @ApiParam(name = "page", value = "Number Page. Type int", required = true, example = "10")
             @RequestParam("page") int page,
             @ApiParam(name = "size", value = "Number of records per page.Type int." +
