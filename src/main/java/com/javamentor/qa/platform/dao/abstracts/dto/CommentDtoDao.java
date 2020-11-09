c52150f90b1c78b655f3681ada9ade07171352a5
monster <nexx01@mail.ru>
2020-11-09 10:39:31 +0300

    delete

--- src/main/java/com/javamentor/qa/platform/dao/abstracts/dto/CommentDtoDao.java
+++
@@ -1,9 +0,0 @@
-package com.javamentor.qa.platform.dao.abstracts.dto;
-
-import com.javamentor.qa.platform.models.dto.CommentDto;
-
-import java.util.Optional;
-
-public interface CommentDtoDao {
-    Optional<CommentDto> getCommentDtoById(Long commentId);
-}
--- src/main/java/com/javamentor/qa/platform/service/abstracts/model/CommentDtoService.java
+++
@@ -1,9 +0,0 @@
-package com.javamentor.qa.platform.service.abstracts.model;
-
-import com.javamentor.qa.platform.models.dto.CommentDto;
-
-import java.util.Optional;
-
-public interface CommentDtoService {
-    Optional<CommentDto> getCommentDtoById(Long commentId);
-}
