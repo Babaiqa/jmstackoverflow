c62e371833f7cb155968fcf6ded68117087b865d
Alexander <sanek.bel@mail.ru>
2020-11-09 22:55:42 +0400

    commit1

--- src/main/java/com/javamentor/qa/platform/webapp/configs/MvcConfig.java
+++ src/main/java/com/javamentor/qa/platform/webapp/configs/MvcConfig.java
@@ -14,6 +14,7 @@ public class MvcConfig implements WebMvcConfigurer {
         registry.addViewController("/ask").setViewName("askQuestion");
         registry.addViewController("/site").setViewName("headerSidebarFooter");
         registry.addViewController("/users").setViewName("fragment/users");
+        registry.addViewController("/main").setViewName("fragment/main");
 
     }
 
