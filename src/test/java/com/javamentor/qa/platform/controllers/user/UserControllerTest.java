feec2d044ce8892b53adb595b1f39d1c93dc575d
Vadim Bridnya <v-bridnya@yandex.ru>
2020-11-10 13:23:32 +0300

    UserControllerTest second commit revision integration test

--- src/test/java/com/javamentor/qa/platform/controllers/user/UserControllerTest.java
+++ src/test/java/com/javamentor/qa/platform/controllers/user/UserControllerTest.java
@@ -2,6 +2,7 @@ package com.javamentor.qa.platform.controllers.user;
 
 import com.github.database.rider.core.api.dataset.DataSet;
 import com.javamentor.qa.platform.AbstractIntegrationQuestionControllerTest;
+import com.javamentor.qa.platform.models.dto.UserDto;
 import com.javamentor.qa.platform.models.dto.UserRegistrationDto;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -10,7 +11,7 @@ import org.springframework.test.web.servlet.MockMvc;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
 public class UserControllerTest extends AbstractIntegrationQuestionControllerTest {
 
@@ -20,15 +21,30 @@ public class UserControllerTest extends AbstractIntegrationQuestionControllerTes
     @Test
     @DataSet(value = "dataset/user/userApi.yml", disableConstraints = true, cleanBefore = true, cleanAfter = true)
     public void shouldGetUserById() throws Exception {
-        this.mockMvc.perform(get("/api/user/1"))
+        UserDto user = new UserDto();
+        user.setId(1L);
+        user.setEmail("ivanov@mail.ru");
+        user.setFullName("Teat");
+        user.setLinkImage("https://www.google.com/search?q=D0");
+        user.setReputation(2);
+
+        this.mockMvc.perform(get("/api/user/" + user.getId()))
                 .andDo(print())
+                .andExpect(content().contentType("application/json"))
+                .andExpect(jsonPath("id").value(user.getId()))
+                .andExpect(jsonPath("email").value(user.getEmail()))
+                .andExpect(jsonPath("fullName").value(user.getFullName()))
+                .andExpect(jsonPath("linkImage").value(user.getLinkImage()))
+                .andExpect(jsonPath("reputation").value(user.getReputation()))
                 .andExpect(status().isOk());
     }
     @Test
     @DataSet(value = "dataset/question/usersQuestionApi.yml", disableConstraints = true, cleanBefore = true, cleanAfter = true)
     public void shouldGetUserByIsNot() throws Exception {
-        this.mockMvc.perform(get("/api/user/4"))
+        int id = 4;
+        this.mockMvc.perform(get("/api/user/" + id))
                 .andDo(print())
+                .andExpect(content().string("User with id " + id + " not found"))
                 .andExpect(status().isBadRequest());
     }
 
@@ -44,8 +60,13 @@ public class UserControllerTest extends AbstractIntegrationQuestionControllerTes
 
         this.mockMvc.perform(post("/api/user/registration").content(jsonRequest).contentType(MediaType.APPLICATION_JSON))
                 .andDo(print())
+                .andExpect(content().contentType("application/json"))
+                .andExpect(jsonPath("id").exists())
+                .andExpect(jsonPath("email").value(user.email))
+                .andExpect(jsonPath("fullName").value(user.fullName))
+                .andExpect(jsonPath("linkImage").isEmpty())
+                .andExpect(jsonPath("reputation").value(0))
                 .andExpect(status().isOk());
-
     }
 
     @Test
@@ -58,6 +79,7 @@ public class UserControllerTest extends AbstractIntegrationQuestionControllerTes
         String jsonRequest = objectMapper.writeValueAsString(user);
         this.mockMvc.perform(post("/api/user/registration").content(jsonRequest).contentType(MediaType.APPLICATION_JSON))
                 .andDo(print())
+                .andExpect(content().string("User with email " + user.getEmail() + " already exist"))
                 .andExpect(status().isBadRequest());
     }
 
@@ -70,6 +92,7 @@ public class UserControllerTest extends AbstractIntegrationQuestionControllerTes
         String jsonRequest = objectMapper.writeValueAsString(user);
         this.mockMvc.perform(post("/api/user/registration").content(jsonRequest).contentType(MediaType.APPLICATION_JSON))
                 .andDo(print())
+                .andExpect(content().string("createUser.userRegistrationDto.email: Заданный email не может существовать"))
                 .andExpect(status().isBadRequest());
     }
 }
