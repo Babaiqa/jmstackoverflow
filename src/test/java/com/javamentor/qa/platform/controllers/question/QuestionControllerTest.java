5adb299106017622bdc6632a1b34d9dac0b1c6d9
Alexander <sanek.bel@mail.ru>
2020-11-20 13:01:44 +0400

    commit2

--- src/test/java/com/javamentor/qa/platform/controllers/question/QuestionControllerTest.java
+++ src/test/java/com/javamentor/qa/platform/controllers/question/QuestionControllerTest.java
@@ -3,18 +3,21 @@ package com.javamentor.qa.platform.controllers.question;
 import com.github.database.rider.core.api.dataset.DataSet;
 import com.javamentor.qa.platform.AbstractIntegrationTest;
 import com.javamentor.qa.platform.models.dto.TagDto;
+import com.javamentor.qa.platform.models.entity.question.Question;
+import com.javamentor.qa.platform.models.entity.question.Tag;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
 @DataSet(value = {"dataset/question/roleQuestionApi.yml",
         "dataset/question/usersQuestionApi.yml",
@@ -34,7 +37,7 @@ class QuestionControllerTest extends AbstractIntegrationTest {
     }
 
     @Test
-    public void shouldSetTagForQuestionIsOk() throws Exception {
+    public void shouldSetTagForQuestionOneTag() throws Exception {
 
         List<TagDto> tag = new ArrayList<>();
         tag.add(new TagDto(1L, "java"));
@@ -48,7 +51,7 @@ class QuestionControllerTest extends AbstractIntegrationTest {
     }
 
     @Test
-    public void shouldSetTagForQuestionIsBadRequest() throws Exception {
+    public void shouldSetTagForQuestionWrongId() throws Exception {
 
         List<TagDto> tag = new ArrayList<>();
         tag.add(new TagDto(1L, "java"));
@@ -61,6 +64,46 @@ class QuestionControllerTest extends AbstractIntegrationTest {
                 .andDo(print())
                 .andExpect(content().string("Question not found"))
                 .andExpect(status().isBadRequest());
+    }
+
+
+    @Test
+    public void shouldSetTagForQuestionFewTag() throws Exception {
+
+        List<TagDto> tag = new ArrayList<>();
+        tag.add(new TagDto(1L, "java"));
+        tag.add(new TagDto(2L, "javaScript"));
+        tag.add(new TagDto(3L, "html"));
+        String jsonRequest = objectMapper.writeValueAsString(tag);
+        this.mockMvc.perform(MockMvcRequestBuilders
+                .patch("/api/question/1/tag/add")
+                .content(jsonRequest)
+                .accept(MediaType.APPLICATION_JSON)
+                .contentType(MediaType.APPLICATION_JSON))
+                .andExpect(status().isOk());
+
+    }
+
+    @Test
+    public void shouldSetTagForQuestionNoTitle() throws Exception {
+
+        Question question = new Question();
+        question.setId(1L);
+        question.setTitle("");
+        question.setDescription("Stream filter on list keeping some of the filtered values");
+        question.setViewCount(3);
+
+
+        List<TagDto> tag = new ArrayList<>();
+        tag.add(new TagDto(1L, "java"));
+        String jsonRequest = objectMapper.writeValueAsString(tag);
+        this.mockMvc.perform(MockMvcRequestBuilders
+                .patch("/api/question/1/tag/add")
+                .content(jsonRequest)
+                .accept(MediaType.APPLICATION_JSON)
+                .contentType(MediaType.APPLICATION_JSON))
+                .andExpect(status().isOk());
+
     }
 
 }
