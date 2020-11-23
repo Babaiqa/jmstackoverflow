128d2629a8e58427bd911845602e53098af55782
Vittiv <utsintercorp@gmail.com>
2020-11-23 07:16:13 +0300

    исправил getPaginationPopular

--- src/main/java/com/javamentor/qa/platform/dao/impl/model/QuestionDaoImpl.java
+++ src/main/java/com/javamentor/qa/platform/dao/impl/model/QuestionDaoImpl.java
@@ -5,38 +5,36 @@ import com.javamentor.qa.platform.dao.abstracts.model.QuestionDao;
 import com.javamentor.qa.platform.models.entity.question.Question;
 import com.javamentor.qa.platform.models.entity.question.Tag;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
 
+import javax.persistence.EntityManager;
 import javax.transaction.Transactional;
-import java.util.Collections;
-import java.util.Comparator;
 import java.util.List;
 
 
 @Repository
 public class QuestionDaoImpl extends ReadWriteDaoImpl<Question, Long> implements QuestionDao {
 
+    private final EntityManager entityManager;
+
+    @Autowired
+    public QuestionDaoImpl(EntityManager entityManager) {
+        this.entityManager = entityManager;
+    }
 
     @Override
     public List<Tag> getAllTagOfQuestion(Question question) {
         return question.getTags();
     }
 
+    @SuppressWarnings("unchecked")
     @Transactional
     public List<Question> getPaginationPopular(int page, int size) {
-        List<Question> list = getAll();
-
-        class CompareQuestionByViewCount implements Comparator<Question>  {
-            public int compare(Question q1, Question q2) {
-                return q2.getViewCount() - q1.getViewCount();
-            }
-        }
-
-        list.sort(Collections.reverseOrder(new CompareQuestionByViewCount()));
-        if (list.size() < page * size) {
-           list.clear();
-           return list;
-        }else return list.subList(page * size - size, page * size);
+         return entityManager.createQuery("SELECT q from Question q order by q.viewCount desc ")
+                 .setFirstResult(page * size - size)
+                 .setMaxResults(size)
+                 .getResultList();
     }
 
 }
