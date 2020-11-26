fb22bbe3ba0dce0fb45fb1b525edf7cd8679cd2f
i210350 <i210350@yandex.ru>
2020-11-26 17:02:18 +0300

    1

--- src/test/java/com/javamentor/qa/platform/controllers/question/QuestionControllerTest.java
+++ src/test/java/com/javamentor/qa/platform/controllers/question/QuestionControllerTest.java
@@ -2,15 +2,15 @@ package com.javamentor.qa.platform.controllers.question;
 
 import com.github.database.rider.core.api.dataset.DataSet;
 import com.javamentor.qa.platform.AbstractIntegrationTest;
-import com.javamentor.qa.platform.dao.abstracts.model.QuestionDao;
-import com.javamentor.qa.platform.models.dto.PageDto;
-import com.javamentor.qa.platform.models.dto.QuestionDto;
-import org.junit.Assert;
+import com.javamentor.qa.platform.models.dto.QuestionCreateDto;
+import com.javamentor.qa.platform.models.dto.TagDto;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+
+import java.awt.print.Book;
 import java.util.ArrayList;
 import java.util.List;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
@@ -27,118 +27,39 @@ import static org.springframework.test.web.servlet.request.MockMvcRequestBuilder
         "dataset/question/tagQuestionApi.yml",
         "dataset/question/question_has_tagQuestionApi.yml",
         "dataset/question/votes_on_question.yml"}, cleanBefore = true, cleanAfter = true)
+
+
+
 class QuestionControllerTest extends AbstractIntegrationTest {
 
     @Autowired
     private MockMvc mockMvc;
 
+
     @Test
-    public void shouldGetLastTenQuestions() throws Exception {
+    public  void shouldAddQuestionStatusOk() throws Exception {
 
-        PageDto<QuestionDto, Object> expected = new PageDto<>();
-        expected.setCurrentPageNumber(1);
-        expected.setItemsOnPage(10);
-        expected.setTotalPageCount(1);
-        expected.setItemsOnPage(10);
-        expected.setTotalResultCount(9);
-        expected.setItems(new ArrayList<>());
+        QuestionCreateDto questionCreateDto = new QuestionCreateDto();
+        questionCreateDto.setUserId(629L);
+        questionCreateDto.setTitle("Question Title49");
+        questionCreateDto.setDescription("Question Description49");
+        List<TagDto> listTagsAdd = new ArrayList<>();
+        listTagsAdd.add(new TagDto(2L, "Tag Name0"));
+        questionCreateDto.setTags(listTagsAdd);
 
-        mockMvc.perform(get("/api/question/order/new")
-                .param("page", "1")
-                .param("size", "10"))
+        String jsonRequest = objectMapper.writeValueAsString(questionCreateDto);
+
+        this.mockMvc.perform(MockMvcRequestBuilders
+                .post("/api/question/add")
+                .content(jsonRequest)
+                .accept(MediaType.APPLICATION_JSON))
                 .andDo(print())
-                .andExpect(status().isOk())
-                .andExpect(content().contentType(MediaType.APPLICATION_JSON))
-                .andExpect(jsonPath("$.currentPageNumber").value(1))
-                .andExpect(jsonPath("$.totalPageCount").isNotEmpty())
-                .andExpect(jsonPath("$.totalResultCount").isNotEmpty())
-                .andExpect(jsonPath("$.items").isNotEmpty())
-                .andExpect(jsonPath("$.itemsOnPage").value( 10 ));
+                .andExpect(status().isOk());
     }
 
     @Test
-    public void shouldReturnErrorMessageBadParameterNegativePage() throws Exception {
-        mockMvc.perform(get("/api/question/order/new")
-                .param("page", "-1")
-                .param("size", "10"))
-                .andDo(print())
-                .andExpect(status().is4xxClientError())
-                .andExpect(content().contentTypeCompatibleWith("text/plain;charset=UTF-8"))
-                .andExpect(content().string("Номер страницы и размер должны быть положительными. Максимальное количество записей на странице 100"));
-    }
-
-    @Test
-    public void shouldReturnErrorMessageBadParameterNegativeSize() throws Exception {
-        mockMvc.perform(get("/api/question/order/new")
-                .param("page", "1")
-                .param("size", "-1"))
-                .andDo(print())
-                .andExpect(status().is4xxClientError())
-                .andExpect(content().contentTypeCompatibleWith("text/plain;charset=UTF-8"))
-                .andExpect(content().string("Номер страницы и размер должны быть положительными. Максимальное количество записей на странице 100"));
-    }
-    @Test
-    public void shouldReturnErrorMessageBadParameterZeroPage() throws Exception {
-        mockMvc.perform(get("/api/question/order/new")
-                .param("page", "0")
-                .param("size", "10"))
-                .andDo(print())
-                .andExpect(status().is4xxClientError())
-                .andExpect(content().contentTypeCompatibleWith("text/plain;charset=UTF-8"))
-                .andExpect(content().string("Номер страницы и размер должны быть положительными. Максимальное количество записей на странице 100"));
-    }
-
-    @Test
-    public void shouldReturnErrorMessageBadParameterZeroSize() throws Exception {
-        mockMvc.perform(get("/api/question/order/new")
-                .param("page", "1")
-                .param("size", "0"))
-                .andDo(print())
-                .andExpect(status().is4xxClientError())
-                .andExpect(content().contentTypeCompatibleWith("text/plain;charset=UTF-8"))
-                .andExpect(content().string("Номер страницы и размер должны быть положительными. Максимальное количество записей на странице 100"));
-    }
-
-    @Test
-    public void shouldReturnErrorMessageBadParameterTooBigSize() throws Exception {
-        mockMvc.perform(get("/api/question/order/new")
-                .param("page", "1")
-                .param("size", "101"))
-                .andDo(print())
-                .andExpect(status().is4xxClientError())
-                .andExpect(content().contentTypeCompatibleWith("text/plain;charset=UTF-8"))
-                .andExpect(content().string("Номер страницы и размер должны быть положительными. Максимальное количество записей на странице 100"));
-    }
-
-    @Test
-    public void shouldReturnEmptyPageCouseOfTooHighPage() throws Exception {
-
-        PageDto<QuestionDto, Object> expected = new PageDto<>();
-        expected.setCurrentPageNumber(100);
-        expected.setItemsOnPage(10);
-        expected.setTotalPageCount(1);
-        expected.setItemsOnPage(10);
-        expected.setTotalResultCount(9);
-
-        List<QuestionDto> expectedItems = new ArrayList<>();
-        expected.setItems(expectedItems);
-
-        String resultContext = mockMvc.perform(get("/api/question/order/new")
-                .param("page", "100")
-                .param("size", "10"))
-                .andDo(print())
-                .andExpect(status().isOk())
-                .andExpect(content().contentType(MediaType.APPLICATION_JSON))
-                .andExpect(jsonPath("$.currentPageNumber").isNotEmpty())
-                .andExpect(jsonPath("$.totalPageCount").isNotEmpty())
-                .andExpect(jsonPath("$.totalResultCount").isNotEmpty())
-                .andExpect(jsonPath("$.items").isEmpty())
-                .andReturn().getResponse().getContentAsString();
-
-        PageDto<QuestionDao, Object> actual = objectMapper.readValue(resultContext, PageDto.class);
-
-        Assert.assertEquals(expected.toString(), actual.toString());
-
+    void getAllDto() throws Exception {
+        System.out.println("test");
     }
 
     @Test
