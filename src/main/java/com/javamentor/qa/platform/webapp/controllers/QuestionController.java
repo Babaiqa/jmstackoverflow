c2b9ed4a1efaa40072141c73d7ee8f39b0854f3c
Vsevolod Volgin <volginvs@list.ru>
2021-01-13 16:37:34 +0300

    refactor PaginationQuestionByPopularDaoImpl, QuestionDtoServiceImpl and api/question/popular for operation with different time periods.

--- src/main/java/com/javamentor/qa/platform/webapp/controllers/QuestionController.java
+++ src/main/java/com/javamentor/qa/platform/webapp/controllers/QuestionController.java
@@ -182,13 +182,24 @@ public class QuestionController {
             @ApiParam(name = "size", value = "Number of entries per page.Type int." +
                     " Максимальное количество записей на странице " + MAX_ITEMS_ON_PAGE,
                     example = "10")
-            @RequestParam("size") int size) {
+            @RequestParam("size") int size,
+            @ApiParam(name = "period", value = "Period in which the popularity of question would be evaluated",
+                    required = false, example = "month", allowableValues = "month, week, day", defaultValue  = "day")
+            @RequestParam(value = "period", defaultValue = "day") String period) {
 
         if (page <= 0 || size <= 0 || size > MAX_ITEMS_ON_PAGE) {
             return ResponseEntity.badRequest().body("Номер страницы и размер должны быть " +
                     "положительными. Максимальное количество записей на странице " + MAX_ITEMS_ON_PAGE);
         }
-        PageDto<QuestionDto, Object> resultPage = questionDtoService.getPaginationPopular(page, size);
+
+        PageDto<QuestionDto, Object> resultPage;
+        if(period.equals("week")) {
+            resultPage = questionDtoService.getPaginationPopular(page, size, 7L);
+        } else if (period.equals("month")) {
+            resultPage = questionDtoService.getPaginationPopular(page, size, 30L);
+        } else {
+            resultPage = questionDtoService.getPaginationPopular(page, size, 1L);
+        }
 
         return ResponseEntity.ok(resultPage);
     }
