89fdfbd1bdf65d015c7395133b27fe1c7f125efb
Viktor1 <zestaw2017@gmail.com>
2021-02-11 09:26:25 +0300

    fixed

--- src/test/java/com/javamentor/qa/platform/controllers/answer/AnswerControllerTest.java
+++ src/test/java/com/javamentor/qa/platform/controllers/answer/AnswerControllerTest.java
@@ -37,6 +37,7 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
         "dataset/answer/answerApi.yml",
         "dataset/answer/roleApi.yml",
         "dataset/answer/questionApi.yml",
+        "dataset/answer/votes_on_answers.yml",
         "dataset/question/questionQuestionApi.yml",
         "dataset/question/answerQuestionApi.yml"},
         cleanBefore = true, cleanAfter = false)
@@ -69,7 +70,7 @@ class AnswerControllerTest extends AbstractIntegrationTest {
     public void shouldAddCommentToAnswerResponseCommentDto() throws Exception {
 
         MvcResult result = this.mockMvc.perform(MockMvcRequestBuilders
-                .post("/api/question/4/answer/14/comment")
+                .post("/api/question/4/answer/1/comment")
                 .content("This is very good answer!")
                 .accept(MediaType.APPLICATION_JSON))
                 .andExpect(MockMvcResultMatchers.jsonPath("$.id").exists())
@@ -117,7 +118,25 @@ class AnswerControllerTest extends AbstractIntegrationTest {
     }
 
 
+    @Test
+    @WithMockUser(username = "admin@tut.by", roles = {"ADMIN"})
+    public void shouldMarkAnswerAsHelpful() throws Exception {
 
+        Answer beforeAnswer = (Answer) entityManager.createQuery("From Answer Where id=44").getSingleResult();
+
+        MvcResult result = this.mockMvc.perform(MockMvcRequestBuilders
+                .patch("/api/question/77/answer/44/upVote"))
+                .andExpect(MockMvcResultMatchers.jsonPath("$.id").exists())
+                .andReturn();
+
+        JSONObject object = new JSONObject(result.getResponse().getContentAsString());
+
+        Answer afterAnswer = (Answer) entityManager.createQuery("From Answer Where id=44").getSingleResult();
+
+        Assert.assertFalse(beforeAnswer.getIsHelpful());
+        Assert.assertEquals(object.get("userId"),4);
+        Assert.assertTrue(afterAnswer.getIsHelpful());
+    }
 
     @Test
     void userVotedForAnswerStatusOk() throws Exception{
@@ -144,27 +163,6 @@ class AnswerControllerTest extends AbstractIntegrationTest {
                 .andExpect(status().isBadRequest())
                 .andExpect(content().contentType("text/plain;charset=UTF-8"))
                 .andExpect(content().string("Question not found"));
-    }
-
-
-    @Test
-    @WithMockUser(username = "admin@tut.by", roles = {"ADMIN"})
-    public void shouldMarkAnswerAsHelpful() throws Exception {
-
-        Answer beforeAnswer = (Answer) entityManager.createQuery("From Answer Where id=44").getSingleResult();
-
-        MvcResult result = this.mockMvc.perform(MockMvcRequestBuilders
-                .patch("/api/question/77/answer/44/upVote"))
-                .andExpect(MockMvcResultMatchers.jsonPath("$.id").exists())
-                .andReturn();
-
-        JSONObject object = new JSONObject(result.getResponse().getContentAsString());
-
-        Answer afterAnswer = (Answer) entityManager.createQuery("From Answer Where id=44").getSingleResult();
-
-        Assert.assertFalse(beforeAnswer.getIsHelpful());
-        Assert.assertEquals(object.get("userId"),4);
-        Assert.assertTrue(afterAnswer.getIsHelpful());
     }
 
 }
