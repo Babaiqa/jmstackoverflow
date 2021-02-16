0cae58199519f3dd8afc1f507e3145cb89ddb174
Viktor1 <zestaw2017@gmail.com>
2021-02-16 12:52:03 +0300

    commit 2

--- src/main/java/com/javamentor/qa/platform/service/abstracts/model/VoteAnswerService.java
+++ src/main/java/com/javamentor/qa/platform/service/abstracts/model/VoteAnswerService.java
@@ -1,13 +1,9 @@
 package com.javamentor.qa.platform.service.abstracts.model;
 
-import com.javamentor.qa.platform.models.entity.question.answer.Answer;
+
 import com.javamentor.qa.platform.models.entity.question.answer.VoteAnswer;
-import com.javamentor.qa.platform.models.entity.user.User;
 
 
 public interface VoteAnswerService extends ReadWriteService<VoteAnswer, Long> {
-
-    boolean isUserAlreadyVotedUp(Answer answer, User user);
-    boolean isUserAlreadyVotedDown(Answer answer, User user);
 
 }
