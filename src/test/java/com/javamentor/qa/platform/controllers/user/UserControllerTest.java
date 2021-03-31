9ef38609fb4f26afa82ca3fd614aae25fe19f125
AndreySazonov <i.andrey.sazonov@yandex.ru>
2021-03-31 22:18:02 +0300

    fix_requestUserTagReputationOverMonth(emptyItems)

--- src/test/java/com/javamentor/qa/platform/controllers/user/UserControllerTest.java
+++ src/test/java/com/javamentor/qa/platform/controllers/user/UserControllerTest.java
@@ -258,11 +258,11 @@ class UserControllerTest extends AbstractIntegrationTest {
         expected.setItemsOnPage(10);
 
         List<UserDtoList> expectedItems = new ArrayList<>();
-        expectedItems.add(new UserDtoList(1L, "Teat", null, 0, Arrays.asList(new TagDto[]{new TagDto(1L, "java", "description"), new TagDto(3L, "html", "description")})));
-        expectedItems.add(new UserDtoList(2L, "Tot", null, 0, Arrays.asList(new TagDto[]{new TagDto(1L, "java", "description"), new TagDto(2L, "javaScript", "description"), new TagDto(5L, "sql", "description")})));
-        expectedItems.add(new UserDtoList(3L, "Tot", null, 0, Arrays.asList(new TagDto[]{new TagDto(5L, "sql", "description")})));
-        expectedItems.add(new UserDtoList(4L, "Tot", null, 0, Arrays.asList(new TagDto[]{})));
-        expectedItems.add(new UserDtoList(5L, "Tot", null, 0, Arrays.asList(new TagDto[]{})));
+        expectedItems.add(new UserDtoList(1L, "Teat", null, 2, Arrays.asList(new TagDto[]{new TagDto(1L, "java", "Java is a popular high-level programming language."), new TagDto(3L, "html", "HTML (HyperText Markup Language) is the markup language for creating web pages and other information to be displayed in a web browser.")})));
+        expectedItems.add(new UserDtoList(2L, "Tot", null, 2, Arrays.asList(new TagDto[]{new TagDto(1L, "java", "Java is a popular high-level programming language."), new TagDto(2L, "javaScript", "For questions regarding programming in ECMAScript (JavaScript/JS) and its various dialects/implementations (excluding ActionScript)."), new TagDto(5L, "sql", "Structured Query Language (SQL) is a language for querying databases.")})));
+        expectedItems.add(new UserDtoList(3L, "Tot", null, 2, Arrays.asList(new TagDto[]{new TagDto(5L, "sql", "Structured Query Language (SQL) is a language for querying databases.")})));
+        expectedItems.add(new UserDtoList(4L, "Tot", null, 2, Arrays.asList(new TagDto[]{})));
+        expectedItems.add(new UserDtoList(5L, "Tot", null, 2, Arrays.asList(new TagDto[]{})));
         expected.setItems(expectedItems);
         // Expected a non-empty value at JSON path "$.items" but found: []
         String resultContext =
