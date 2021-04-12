6079be6d3b517f95645252ec08780d5561533d14
Alex Efremov <sneg1203@mail.ru>
2021-04-12 12:14:21 +0300

    add pagination size and page

--- src/main/java/com/javamentor/qa/platform/dao/impl/dto/pagination/chats/PaginationMessageDaoImpl.java
+++ src/main/java/com/javamentor/qa/platform/dao/impl/dto/pagination/chats/PaginationMessageDaoImpl.java
@@ -19,7 +19,11 @@ public class PaginationMessageDaoImpl implements PaginationDao<MessageDto> {
 
     @Override
     public List<MessageDto> getItems(Map<String, Object> parameters) {
+
+        int page = (int)parameters.get("page");
+        int size = (int)parameters.get("size");
         long chatId = (long) parameters.get("chatId");
+
 
         return (List<MessageDto>) entityManager.unwrap(Session.class)
                 .createQuery("SELECT new com.javamentor.qa.platform.models.dto.MessageDto(" +
@@ -32,6 +36,8 @@ public class PaginationMessageDaoImpl implements PaginationDao<MessageDto> {
                         "from Message ms  where ms.chat.id =: chatId order by ms.persistDate desc")
                 .unwrap(Query.class)
                 .setParameter("chatId", chatId)
+                .setFirstResult(page * size - size)
+                .setMaxResults(size)
                 .getResultList();
 
     }
