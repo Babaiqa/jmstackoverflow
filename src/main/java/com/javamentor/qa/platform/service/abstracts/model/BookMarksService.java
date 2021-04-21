960106696c59122380a17f5277374ecd047df08f
ruslantech <ruslantech1@gmail.com>
2021-04-22 00:39:02 +0600

    commit

---
+++ src/main/java/com/javamentor/qa/platform/service/abstracts/model/BookMarksService.java
@@ -0,0 +1,6 @@
+package com.javamentor.qa.platform.service.abstracts.model;
+
+import com.javamentor.qa.platform.models.entity.BookMarks;
+
+public interface BookMarksService extends ReadWriteService<BookMarks, Long>{
+}
