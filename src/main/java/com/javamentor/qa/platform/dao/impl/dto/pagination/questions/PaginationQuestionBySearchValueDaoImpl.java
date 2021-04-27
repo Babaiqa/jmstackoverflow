328be315ebc4538f7dcdd34363a62df9166771ff
Andrei Dik <andrej.dik.02@gmail.com>
2021-04-27 10:55:15 +0300

    Revert "Merge branch 'VoteBugFix' into 'dev'"
    This reverts merge request !344

--- src/main/java/com/javamentor/qa/platform/dao/impl/dto/pagination/questions/PaginationQuestionBySearchValueDaoImpl.java
+++ src/main/java/com/javamentor/qa/platform/dao/impl/dto/pagination/questions/PaginationQuestionBySearchValueDaoImpl.java
@@ -75,7 +75,7 @@ public class PaginationQuestionBySearchValueDaoImpl implements PaginationDao<Que
                 "question.description as question_description," +
                 "question.viewCount as question_viewCount," +
                 "(select count(a.question.id) from Answer a where a.question.id=question_id) as question_countAnswer," +
-                "coalesce((select sum(v.vote) from VoteQuestion v where v.question.id = question.id), 0) as question_countValuable," +
+                "(select count(v.question.id) from VoteQuestion v where v.question.id=question_id) as question_countValuable," +
                 "question.persistDateTime as question_persistDateTime," +
                 "question.lastUpdateDateTime as question_lastUpdateDateTime, " +
                 "tag.id as tag_id,tag.name as tag_name, tag.description as tag_description " +
--- src/main/java/com/javamentor/qa/platform/dao/impl/dto/pagination/questions/PaginationQuestionWithoutAnswersNoAnyAnswerDaoImpl.java
+++ src/main/java/com/javamentor/qa/platform/dao/impl/dto/pagination/questions/PaginationQuestionWithoutAnswersNoAnyAnswerDaoImpl.java
@@ -40,7 +40,7 @@ public class PaginationQuestionWithoutAnswersNoAnyAnswerDaoImpl implements Pagin
                         "question.description as question_description," +
                         " question.viewCount as question_viewCount," +
                         "(select count(a.id) from Answer a where a.question.id=question.id) as question_countAnswer," +
-                        "coalesce((select sum(v.vote) from VoteQuestion v where v.question.id = question.id), 0) as question_countValuable," +
+                        "(select count(v.id) from VoteQuestion v where v.question.id=question.id) as question_countValuable," +
                         "question.persistDateTime as question_persistDateTime," +
                         "question.lastUpdateDateTime as question_lastUpdateDateTime, " +
                         " tag.id as tag_id,tag.name as tag_name, tag.description as tag_description " +
